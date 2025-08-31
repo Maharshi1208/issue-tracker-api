63c41812d5c8b1e993b072f4a785f33fbafb826d
maharshi012 <maharshipurohit1999@gmail.com>
2025-08-31 18:26:12 -0400

    test(ci): ignore client/ in jest; scope tsc to API only

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,28 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  // Only pick up API tests
+  testMatch: ['**/tests/**/*.test.ts'],
+
+  // Transform TS (not TSX) and use the root tsconfig (API only)
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.json', isolatedModules: true }],
+  },
+
+  moduleFileExtensions: ['ts', 'js', 'json'],
+
+  // Make sure nothing under client/ is touched by Jest
+  testPathIgnorePatterns: ['/node_modules/', '/client/'],
+  modulePathIgnorePatterns: ['/client/'],
+  coveragePathIgnorePatterns: ['/client/'],
+
+  // Don’t fail the build on TS diagnostics for tests that run
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+      diagnostics: { warnOnly: true },
+    },
   },
 };
